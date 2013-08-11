67c4a3f379612d70d598c9d533ec23e6fcd488f1
crazy2be <crazy1be@gmail.com>
2013-08-11 16:25:27 -0400

    Cleanup RLE implementation.

--- RLE/justin.h
+++ RLE/justin.h
@@ -6,20 +6,21 @@ class JustinRLE : public RLE {
 public:
 	JustinRLE() {};
 	virtual ~JustinRLE() {};
+
 	virtual vector<bool> encode(vector<bool> decoded) {
 		vector<bool> encoded;
 		bool bit = decoded[0];
 		encoded.push_back(bit);
 		int offset = 0;
 		while (offset < decoded.size()) {
-			int len = findLongest(decoded.begin() + offset, decoded.end());
+			int len = findLongestRun(decoded.begin() + offset, decoded.end());
 			encodeInt(encoded, len);
 			offset += len;
-			cout << len << " " << offset << " " << encoded << endl;
 		}
 		return encoded;
 	}
-	int findLongest(iter start, iter end) {
+
+	int findLongestRun(iter start, iter end) {
 		bool first = *start;
 		int len = 0;
 		for (; start != end; start++) {
@@ -28,9 +29,7 @@ public:
 		}
 		return len;
 	}
-	int log2(int n) {
-		return int(log(double(n)) / log(2));
-	}
+
 	void encodeInt(vector<bool>& to, int n) {
 		int len = log2(n);
 		for (int i = 0; i < len; i++) {
@@ -40,6 +39,27 @@ public:
 			to.push_back(bool(n & (1 << (len - i))));
 		}
 	}
+	int log2(int n) {
+		return int(log(double(n)) / log(2));
+	}
+
+	virtual vector<bool> decode(vector<bool> encoded) {
+		vector<bool> decoded;
+		bool bit = encoded[0];
+		int offset = 1;
+		while (offset < encoded.size()) {
+			int readLen = -1;
+			int runLen = -1;
+			decodeInt(encoded.begin() + offset, encoded.end(), readLen, runLen);
+			for (int i = 0; i < runLen; i++) {
+				decoded.push_back(bit);
+			}
+			bit = !bit;
+			offset += readLen;
+		}
+		return decoded;
+	}
+
 	void decodeInt(iter start, iter end, int& readLen, int& result) {
 		int len = 1;
 		readLen = 0;
@@ -48,29 +68,10 @@ public:
 		}
 		result = 0;
 		for (int i = 0; i < len; i++) {
-			cout << result << " " << *start << " " << i << endl;
 			result <<= 1;
 			result |= int(*start);
 			start++;
 			readLen++;
 		}
 	}
-	virtual vector<bool> decode(vector<bool> encoded) {
-		vector<bool> decoded;
-		bool bit = encoded[0];
-		int offset = 1;
-		while (offset < encoded.size()) {
-			int readLen = -1, runLen = -1;
-			decodeInt(encoded.begin() + offset, encoded.end(), readLen, runLen);
-			for (int i = 0; i < runLen; i++) {
-				decoded.push_back(bit);
-			}
-			bit = !bit;
-			offset += readLen;
-			cout << bit << " " << readLen << " " << offset << " " << runLen << " " << decoded << endl;
-		}
-		return decoded;
-	}
-
-private:
 };
